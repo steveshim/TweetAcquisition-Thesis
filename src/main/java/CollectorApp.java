f52586ba13d9c3fe35a88dd4d96e98ba29ed23e9
Steve Shim <steveyshim@gmail.com>
2016-10-08 07:05:24 -0700

    began implementation of collection data

--- src/main/java/CollectorApp.java
+++ src/main/java/CollectorApp.java
@@ -3,6 +3,7 @@ import acquisition.Source;
 public class CollectorApp {
 
     public static void main(String[] args){
-        Source source = new Source(Long.MAX_VALUE, "moviename");
+        Source source = new Source(Long.MAX_VALUE, "moviename", "2015-10-11");
+
     }
 }
--- src/main/java/acquisition/Source.java
+++ src/main/java/acquisition/Source.java
@@ -1,24 +1,48 @@
 package acquisition;
 
+import com.google.common.collect.Lists;
 import model.TwitterResponse;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
+
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class Source implements Iterator<Collection<TwitterResponse>> {
 
     private long minId;
-    private final String QUERY;
+    private final String TWITTER_QUERY;
+    //date must be in "YYYY-MM-DD" format
+    private final String MOVIE_DATE;
+    private final String TWITTER_CONSUMER_KEY = System.getenv("CONSUMER_KEY");
+    private final String TWITTER_CONSUMER_SECRET = System.getenv("CONSUMER_SECRET");
+    private final String TWITTER_ACCESS_TOKEN = System.getenv("ACCESS_TOKEN");
+    private final String TWITTER_ACCESS_SECRET = System.getenv("ACCESS_SECRET");
 
 
-    public Source(long minId, String query){
-        System.out.println("Searching for: " + query);
+    public Source(long minId, String searchQuery, String date){
+        System.out.println("Searching for: " + searchQuery);
         this.minId = minId;
-        this.QUERY = query;
+        this.TWITTER_QUERY = searchQuery;
+        this.MOVIE_DATE = date;
     }
 
     public Collection<TwitterResponse> next(){
-        return null;
+        List<TwitterResponse> list = Lists.newArrayList();
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+
+        cb.setDebugEnabled(true).setOAuthConsumerKey(TWITTER_CONSUMER_KEY)
+                .setOAuthConsumerSecret(TWITTER_CONSUMER_SECRET).setOAuthAccessToken(TWITTER_ACCESS_TOKEN)
+                .setOAuthAccessTokenSecret(TWITTER_ACCESS_SECRET);
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+
+        Query query = new Query(TWITTER_QUERY);
+        query.setLang("en");
+        query.setSince(MOVIE_DATE);
+        return list;
     }
 
     public boolean hasNext(){
