47ec761c7f6c32607b9e978c6f466dd428778921
Steve Shim <steveyshim@gmail.com>
2016-10-16 10:30:41 -0700

    added to app to save

--- src/main/java/CollectorApp.java
+++ src/main/java/CollectorApp.java
@@ -1,13 +1,19 @@
+import acquisition.Collector;
 import acquisition.Source;
+import model.TwitterResponse;
+
+import java.util.Collection;
 
 public class CollectorApp {
 
     public static void main(String[] args){
 
+        Collector collector = new Collector();
         Source source = new Source(Long.MAX_VALUE, "#StayPeculiar", "2016-09-30");
 
         while (source.hasNext()){
-            source.next();
+            Collection<TwitterResponse> tweets = source.next();
+            collector.save(tweets);
         }
 
     }
