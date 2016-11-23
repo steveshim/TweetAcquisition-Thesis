bc95a5ae59e85dc43699c9be044d8b0fc6717774
Steve Shim <steveyshim@gmail.com>
2016-11-22 17:24:19 -0800

    new week of movies

--- src/main/java/CollectorApp.java
+++ src/main/java/CollectorApp.java
@@ -8,9 +8,9 @@ public class CollectorApp {
 
     public static void main(String[] args){
 
-        double gross = 24074047.0;
-        String mondayDate = "2016-11-14";
-        String hashtag = "#ArrivalMovie";
+        double gross = 74403387.0;
+        String mondayDate = "2016-11-21";
+        String hashtag = "#FantasticBeasts";
 
         Collector collector = new Collector();
         Source source = new Source(Long.MAX_VALUE, hashtag, mondayDate, gross);
