1cfefe723aa64766e138879b130819f6405ccd8c
Steve Shim <steveyshim@gmail.com>
2017-02-15 13:28:49 -0800

    max complexity and test on testing set

--- src/main/java/CollectorApp.java
+++ src/main/java/CollectorApp.java
@@ -8,19 +8,18 @@ public class CollectorApp {
 
     public static void main(String[] args){
 
-        double gross = 3775596;
-        String mondayDate = "2017-02-06";
-        String hashtag = "#TheSpaceBetweenUs";
+        double gross = 53003468;
+        String mondayDate = "2017-02-13";
+        String hashtag = "#LEGOBatmanMovie";
         int days = 3;
-        double theaterCount = 2812;
-        double averageTemp = 0;
-        double averageRain = 0;
+        double theaterCount = 4088;
+        double averageTemp = 41.5;
+        double averageRain = 0.06;
         double averageSnow = 0;
 
         Collector collector = new Collector();
         //Source source = new Source(Long.MAX_VALUE, hashtag, mondayDate, gross);
-        Source source = new Source(Long.MAX_VALUE, hashtag, mondayDate, gross, days,
-                theaterCount, averageTemp, averageRain, averageSnow);
+        Source source = new Source(Long.MAX_VALUE, hashtag, mondayDate, gross, days, theaterCount, averageTemp, averageRain, averageSnow);
 
         while (source.hasNext()){
             Collection<TwitterResponse> tweets = source.next();
